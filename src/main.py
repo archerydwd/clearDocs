69451eb6d7ead9d691fe83bb541d0aae7f9ab948
Darren Daly <archerydwd@gmail.com>
2014-11-26 14:03:56 +0000

    link

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+import argparse # this is a python library for taking in arguments from the terminal
+# why are we using python? need to be able to answer this.
+#https://docs.python.org/2/library/argparse.html#module-argparse
+# create 
+parser = argparse.ArgumentParser(description='Process source code files.')
+
 
 def getinput(source):
 	with open(source, "r") as code:
