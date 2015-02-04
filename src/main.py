17405b2ae5298db8411a046fe6c972223fc5be50
mattdalton1 <daltonmatt1@gmail.com>
2015-02-04 14:17:09 +0000

    Identify Class name

--- src/main.py
+++ src/main.py
@@ -39,12 +39,18 @@ def checkLanguage(ext):
 ext = getExt(sys.argv[1])
 if checkLanguage(ext):
 	for line in read_data:
-		if 'public' in line or 'private' in line:
-			print(line)
+#		if 'public' in line or 'private' in line:
+#			print(line)
 		
-		if '*' in line:
-			print(line)
-			
-
+		# Identify name of the class to be used in the class summary section
+		# Ignore 'class' if inside a comment section
+		if 'class' in line and '*' not in line and '//' not in line:
+			print("Class Summary")
+			pos=line.index('class')
+			print(line[pos+5:line.index(" ",pos+5)])
+			print("Access Specifier: ",)
+		
+#		if '*' in line:
+#			print(line)
 else:
 	print("it's not supported")
