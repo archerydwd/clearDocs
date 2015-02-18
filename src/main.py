ac83f4e60cb4b0afac137c03987f84a97022f869
Darren Daly <archerydwd@gmail.com>
2015-02-18 15:38:29 +0000

    Added method summary, attribute summary and getclassname method

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import sys, io, os
 file_dir = os.path.dirname(__file__)
 rel_supported_languages = 'supported-languages.txt'
 abs_supported_languages = os.path.join(file_dir, rel_supported_languages)
+class_name = ""
 
 try:
 	with open(sys.argv[1], "r") as code:
@@ -21,6 +22,11 @@ except:
 def getExt(file_name):
 	my_array = file_name[::-1].split('.')
 	return my_array[0][::-1]
+
+def getClassName(line):
+	pos = line.index('class')
+	words_in_line = line[7:].split()
+	return words_in_line[1]
 
 def checkLanguage(ext):
 	try:
@@ -41,30 +47,40 @@ def checkLanguage(ext):
 ext = getExt(sys.argv[1])
 if checkLanguage(ext):
 	inserted = False
+	comment_file = ""
+	list_of_attributes = []
 	for line in read_data:
 		
 		# Identify name of the class to be used in the class summary section
 		# Ignore 'class' if inside a comment section
 		if 'class' in line and '*' not in line and '//' not in line:
 			comment_file = "Class Summary"
-			pos=line.index('class')
-			wordsof = line[7:].split()
-			comment_file = comment_file + "\n" + wordsof[1]
+			class_name = getClassName(line) 
+			comment_file = comment_file + "\n" + class_name
 			comment_file = comment_file + "\n" + "Access Modifier: "
 			line_items = line.split()
 			comment_file = comment_file + "\n" + line_items[0] + "\n"
-			
-		if 'public' in line or 'private' in line:
+		
+		# Identify methods and attributes adding the methods to the output and storing the attributes to be added after the loop
+		if 'public' in line or 'private' in line or 'protected' in line:
 			if inserted == False: 
-				inserted = True
 				comment_file = comment_file + "\nMethods in file:\n"
-			comment_file = comment_file + line + "\n"
-		
+				inserted = True
+			if class_name not in line and 'public static void main(String[] args)' not in line and '(' in line:
+				comment_file = comment_file + line.lstrip() + "\n"
+	
+			if '(' not in line and class_name not in line:
+				list_of_attributes.append(line)
+	
+	comment_file = comment_file + "\nClass Attributes in file:\n"
+	for line in list_of_attributes:
+		comment_file = comment_file + line.lstrip() + "\n"
+
 	#	if '*' in line or '//' in line:
-	#	 	comment_file = comment_file + "\nComments in file:\n"
-	#	 	comment_file = comment_file + line + "\n"
+	#		comment_file = comment_file + "\nComments in file:\n"
+	#		comment_file = comment_file + line + "\n"
 		
-	with open(wordsof[1] + "Comments.html", 'w') as html_file:
+	with open(class_name + "Comments.html", 'w') as html_file:
 		html_file.write(highlight(comment_file, PythonLexer(), HtmlFormatter()))
 else:
 	print("it's not supported")
